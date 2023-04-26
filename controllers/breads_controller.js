d25993ac71cb90cfb5dd5aaee12835bd98f5d5ee
Benjamin <ben.chesser19@gmail.com>
2023-04-25 18:59:18 -0700

    Added more views

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -5,13 +5,18 @@ const Bread = require('../models/breads.js')
 //INDEX - READ ALL
 breads.get('/', (req,res) => {
     //res.send(Bread);
-    res.render('index')
+    res.render('index', 
+    {
+        breads: Bread
+    });
 });
 
 //READ ONE
 breads.get('/:arrayIndex', (req, res) => {
     const arrayIndex = req.params.arrayIndex;
-    res.send(Bread[arrayIndex]);
+    res.render('Show', {
+        bread: Bread[arrayIndex],
+    });
 });
 
 module.exports = breads;
